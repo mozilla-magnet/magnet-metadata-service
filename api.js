c5c01222019adeacfbd50382bdf6130e73b3a8c7
Sam Giles <sam.e.giles@gmail.com>
2016-01-22 15:05:54 +0000

    Add REST-like API endpoints with some basic validation

--- api.js
+++ api.js
@@ -1,8 +1,43 @@
-var config = require('./config.js');
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const config = require('./config.js');
+const bodyParser = require('body-parser');
 
-var app = express();
+const app = express();
+
 app.use(bodyParser.json());
+
+app.post(/^\/metadata(\/)?$/, function(req, res) {
+  const requestBody = req.body;
+
+  if (!validatePostRequest(req, res)) {
+    return;
+  }
+
+  res.json(requestBody);
+});
+
+app.post(/^\/metadata\/raw(\/)?$/, function(req, res) {
+  const requestBody = req.body;
+
+  if (!validatePostRequest(req, res)) {
+    return;
+  }
+
+  res.json(requestBody);
+});
+
+function validatePostRequest(req, res) {
+
+  // If the content-type header is not 'application/json', the request body parser
+  // ignores the content so it looks like an empty request.
+  const validContentType = req.get("content-type").toLowerCase() == "application/json";
+  if (!validContentType) {
+    res.status(400);
+    res.send("Request must be Content-Type: application/json");
+    return false;
+  }
+
+  return true;
+}
 
 module.exports = app;

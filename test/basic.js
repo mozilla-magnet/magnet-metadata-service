5f98499dca3e89fe9772a4016427e6c3d0259640
Francisco Jordano <fjordano@mozilla.com>
2016-02-08 19:05:53 +0000

    Adding test to check basic output from api, compatible with google web service

---
+++ test/basic.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var request = require('supertest');
+var app = require('../api.js');
+var assert = require('chai').assert;
+
+var basicSites = {
+  objects: [
+    {
+      url: 'https://www.mozilla.org/en-GB/'
+    }
+  ]
+};
+
+describe('Basic parsing', () => {
+  it('should give us compatible output with Google PW service', (done) => {
+    request(app)
+      .post('/metadata/')
+      .send(basicSites)
+      .expect(200)
+      .end((err, response) => {
+        assert.isNull(err);
+
+        var result = JSON.parse(response.text);
+        assert.lengthOf(result, 1);
+
+        result = result[0];
+        // Check basic fields 
+        ['id', 'url', 'displayUrl', 
+          'title', 'description', 'icon'].forEach((field) => {
+          assert.isNotNull(result[field]);
+        });
+
+        assert.equal(result.id, result.url);
+        assert.equal(result.id, result.displayUrl);
+
+        done();
+      });
+  });
+});

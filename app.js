3e81b540403f4b8514536fd9d737cf70df2235a7
Francisco Jordano <arcturus@ardeenelinfierno.com>
2016-06-08 16:25:31 +0100

    Enabling cors in the metadata server (configurable) r=wilsonpage (#47)
    * Enabling cors in the metadata server (configurable) r=wilsonpage
    
    * Single quoting all the things!

--- app.js
+++ app.js
@@ -4,12 +4,21 @@
  */
 
 const express = require('express');
+const config = require('./config');
 const morgan = require('morgan');
 
-module.exports = express()
+const app = express();
+if (config.enable_cors) {
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+}
+
+module.exports = app
   .use(morgan('combined'))
   .use('/api/v1/', require('./lib/routes/api'))
-
   // compatibility for google's physical-web schema
   .post(/^\/resolve-scan$/, (req, res) => res.redirect('/api/v1/metadata'))
   .use('/', require('./lib/routes/frontend'));
